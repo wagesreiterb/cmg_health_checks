119c1a79deb1eee05ab1c6a87d4286bae0defc92
Bernhard Wagesreiter <bernhard.wagesreiter@a1.at>
2024-03-11 12:13:50 +0100

    ci test 010

--- test/test_cmg.py
+++ test/test_cmg.py
@@ -9,7 +9,7 @@ class TestCmgQuery(unittest.TestCase):
 
     def test_login(self):
         # self.assertRaises(NoValidConnectionsError, self.cmg.connect)
-        welcome_message = self.cmg.connect('127.0.0.1')
+        welcome_message = self.cmg.connect('127.0.0.2')
         # self.assertTrue(welcome_message.startswith("Fake SR OS Software"), "Unexpected welcome message")
         self.assertTrue(welcome_message.startswith("Fake OS Software"))
         self.cmg.disconnect()
@@ -21,6 +21,7 @@ class TestCmgQuery(unittest.TestCase):
 
     def test_show_router_9999_bfd_session(self):
         self.cmg.connect('127.0.0.1')
+        # ping_command = 'ping router 6203 source 10.251.74.130 10.251.74.129 count 1'
         ping_command = 'ping router 6203 source 10.251.74.130 10.251.74.129 count 1'
         cmg_output = self.cmg.cmg_output(ping_command)
         result = self.cmg.ping_result(ping_command, cmg_output)
